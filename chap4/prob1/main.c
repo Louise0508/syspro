f8824ee7eced16397f3a790f5a51aa4f70a63a9e
Bae TaeHun <aoossy11@naver.com>
2023-10-11 06:48:50 +0000

    4-2 progrem

--- chap4/prob1/main.c
+++ chap4/prob1/main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[])
-{
-	FILE *fp;
-	int c;
-	if (argc < 2)
-		fp = stdin;
-	else fp = fopen(argv[1], "r");
 
-	c = getc(fp);
-	while (c != EOF) {
-		putc(c, stdout);
-		c = getc(fp);
+{
+	char c;
+	FILE *fp1, *fp2;
+	if (argc != 3) {
+		fprintf(stderr, "How to use: %s File1 File2\n", argv[0]);
+		return 1;
 	}
 
-	fclose(fp);
+	fp1 = fopen(argv[1], "r");
+	if (fp1 == NULL) {
+		fprintf(stderr, "File %s Open Error\n", argv[1]);
+		return 2;
+	}
+
+	fp2 = fopen(argv[2], "w");
+	while ((c = fgetc(fp1)) != EOF)
+		fputc(c, fp2);
+
+	fclose(fp1);
+	fclose(fp2);
 	return 0;
 }
-
