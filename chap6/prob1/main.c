8b01e564c9f755450702bfd87955527025331490
Bae TaeHun <aoossy11@naver.com>
2023-11-01 05:50:49 +0000

    6-1

---
+++ chap6/prob1/main.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	struct stat buf;
+	for (i = 1; i < argc; i++) {
+		printf("%s: ", argv[i]);
+		if (lstat(argv[i], &buf) < 0) {
+			perror("lstat()");
+			continue;
+		}
+		if (S_ISREG(buf.st_mode))
+			printf("%s \n", "normal file");
+		if (S_ISDIR(buf.st_mode))
+			printf("%s \n", "dir");
+		if (S_ISCHR(buf.st_mode))
+			printf("%s \n", "charter file");
+		if (S_ISBLK(buf.st_mode));
+			printf("%s \n", "block file");
+		if (S_ISFIFO(buf.st_mode))
+			printf("%s \n", "FIFO file");
+		if (S_ISLNK(buf.st_mode))
+			printf("%s \n", "symlink");
+		if (S_ISSOCK(buf.st_mode))
+			printf("%s \n", "socket");
+	}
+	exit(0);
+}
