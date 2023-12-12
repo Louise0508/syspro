94b1fbf8ea3166ce4c5a155ae22f03f56da01e43
Louise0508 <aoossy11@naver.com>
2023-12-13 00:22:12 +0900

    11hw

---
+++ chap11/hw1/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void alarmHandler(int signo);
+
+int main() {
+	struct sigaction sa;
+	sa.sa_handler = alarmHandler;
+    	sa.sa_flags = 0;
+    	sigemptyset(&sa.sa_mask);
+
+    	if (sigaction(SIGALRM, &sa, NULL) == -1) {
+        	perror("sigaction");
+        	return 1;
+    	}
+
+    	alarm(5); 
+    	printf("loop\n");
+    		while (1) {
+        		sleep(1);
+        		printf("1sec\n");
+    		}
+
+    	printf("error\n");
+	return 0;
+}
+
+void alarmHandler(int signo) {
+	printf("wake up\n");
+	exit(0);
+}
+
